97919f8343f1b5f14ffd740eb695a5a00bda009b
hanmi <hanmilton.777@gmail.com>
2026-05-09 17:31:04 -0500

    chore(account): initialize empty spring boot project
    Add the default Spring Boot project structure with Gradle wrapper, base configuration, and initial application and test classes.

---
+++ src/test/java/com/pichincha/dm/cuaa/account/AysMsaDmCuaaCrAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pichincha.dm.cuaa.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AysMsaDmCuaaCrAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
