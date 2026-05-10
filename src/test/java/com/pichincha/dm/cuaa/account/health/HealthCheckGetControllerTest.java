2e865dd8d5fc60a16dc4a46da1399673be0dfb41
hanmi <hanmilton.777@gmail.com>
2026-05-10 04:35:43 -0500

    test(health): expect plain ok status response

--- src/test/java/com/pichincha/dm/cuaa/account/health/HealthCheckGetControllerTest.java
+++ src/test/java/com/pichincha/dm/cuaa/account/health/HealthCheckGetControllerTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 class HealthCheckGetControllerTest extends RequestTestCase {
 
     @Test
-    void given_applicationIsRunning_when_getHealthCheck_then_returnStatusOkWithEmoji() throws Exception {
-        assertResponse("/health-check", 200, "{ \"status\": \"ok ✅\" }");
+    void given_applicationIsRunning_when_getHealthCheck_then_returnStatusOk() throws Exception {
+        assertResponse("/health-check", 200, "{ \"status\": \"ok\" }");
     }
 }
