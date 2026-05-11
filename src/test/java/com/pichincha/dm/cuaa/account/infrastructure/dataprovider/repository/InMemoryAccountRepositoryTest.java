7de8e5a0485bbc6a2b8906c0b1ab6b8de60ecd5f
hanmi <hanmilton.777@gmail.com>
2026-05-10 20:42:10 -0500

    test(account): add infrastructure test case hierarchy for repository integration tests

--- src/test/java/com/pichincha/dm/cuaa/account/infrastructure/dataprovider/repository/InMemoryAccountRepositoryTest.java
+++ src/test/java/com/pichincha/dm/cuaa/account/infrastructure/dataprovider/repository/InMemoryAccountRepositoryTest.java
@@ -5,16 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.pichincha.dm.cuaa.account.domain.entities.Account;
-import com.pichincha.dm.cuaa.account.infrastructure.dataprovider.repository.mapper.AccountRepositoryMapperImpl;
 import com.pichincha.dm.cuaa.account.shared.objectmothers.AccountMother;
 import com.pichincha.dm.cuaa.account.shared.objectmothers.AccountNumberMother;
 import org.junit.jupiter.api.Test;
 
-final class InMemoryAccountRepositoryTest {
+final class InMemoryAccountRepositoryTest extends AccountInfrastructureTestCase {
 
     @Test
     void given_validAccount_when_save_then_accountIsPersisted() {
-        InMemoryAccountRepository repository = new InMemoryAccountRepository(new AccountRepositoryMapperImpl());
         Account account = AccountMother.random();
 
         repository.save(account).block();
@@ -25,7 +23,6 @@ final class InMemoryAccountRepositoryTest {
 
     @Test
     void given_savedAccount_when_findByAccountNumber_then_returnAccount() {
-        InMemoryAccountRepository repository = new InMemoryAccountRepository(new AccountRepositoryMapperImpl());
         Account account = AccountMother.random();
 
         repository.save(account).block();
@@ -37,7 +34,6 @@ final class InMemoryAccountRepositoryTest {
 
     @Test
     void given_nonExistingAccountNumber_when_findByAccountNumber_then_returnEmpty() {
-        InMemoryAccountRepository repository = new InMemoryAccountRepository(new AccountRepositoryMapperImpl());
         String nonExistingAccountNumber = AccountNumberMother.create("ACC-0000-NONEXIST-00").getValue();
 
         boolean isPresent = repository.findByAccountNumber(nonExistingAccountNumber).blockOptional().isPresent();
---
+++ src/test/java/com/pichincha/dm/cuaa/account/shared/InfrastructureTestCase.java
@@ -0,0 +1,9 @@
+package com.pichincha.dm.cuaa.account.shared;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public abstract class InfrastructureTestCase {
+}
